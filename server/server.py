fe414e99843a3a85e2c6539fd919069b5e227f5b
Hamza-Elbouzidi <elbouzidihamza5@gmail.com>
2024-10-17 10:30:34 +0200

    second commit

--- server/server.py
+++ server/server.py
@@ -1,15 +1,18 @@
-from typing import Union
-
+import pandas as pd
+import textdistance 
 from fastapi import FastAPI
 
 app = FastAPI()
 
+game_db = pd.read_parquet("../data/game_database.parquet")
 
 @app.get("/")
 def index():
     return {"Hello world"}
 
 
-@app.get("/get_recommendations")
-def read_item(game_name:str):
-    return {"game_name" : game_name}
+@app.get("/search")
+def search(game_name: str):
+    distances = game_db["name"].apply(lambda v : textdistance.levenshtein(v,game_name)).sort_values()
+    return [{"game": game_db.loc[idx].to_json(),"distance" : distance} for idx, distance
+            in distances[:3].items()]
