9a0bbfe2208880bcb98f6506b40207e22b142361
Hamza-Elbouzidi <elbouzidihamza5@gmail.com>
2024-10-23 15:16:26 +0200

    almost done

--- server/server.py
+++ server/server.py
@@ -1,8 +1,23 @@
 import pandas as pd
 import textdistance 
 from fastapi import FastAPI
+from fastapi.middleware.cors import CORSMiddleware
+import json
 
 app = FastAPI()
+
+origins = [
+    "http://localhost",
+    "http://localhost:3000",
+]
+
+app.add_middleware(
+    CORSMiddleware,
+    allow_origins=origins,
+    allow_credentials=True,
+    allow_methods=["*"],
+    allow_headers=["*"],
+)
 
 game_db = pd.read_parquet("../data/game_database.parquet")
 
@@ -12,7 +27,7 @@ def index():
 
 
 @app.get("/search")
-def search(game_name: str):
-    distances = game_db["name"].apply(lambda v : textdistance.levenshtein(v,game_name)).sort_values()
-    return [{"game": game_db.loc[idx].to_json(),"distance" : distance} for idx, distance
+def search(query: str):
+    distances = game_db["name"].apply(lambda v : textdistance.levenshtein(v,query)).sort_values()
+    return [{"game": json.loads(game_db.loc[idx].to_json()),"distance" : distance} for idx, distance
             in distances[:3].items()]
--- webapp/app/page.tsx
+++ webapp/app/page.tsx
@@ -1,8 +1,24 @@
 "use client";
 
 import Head from "next/head";
+import { useState } from "react";
+
+type Game = {
+name : string;
+header_image : string;
+short_description : string;
+detailed_description : string;
+genres : string;
+embedding : number[];
+}
+
+type SearchResults = {
+  game : Game;
+  distance : number;
+}
 
 export default function Home() {
+  const [searchResults, setSearchResults] = useState([] as SearchResults[]);
   return (
     <div>
       <Head>
@@ -18,10 +34,18 @@ export default function Home() {
       <main>
         <div className="content">
         <input type="search" className ="searchbar" placeholder="Enter text" onChange={(e) =>{
-          fetch('api/search').then((res) => res.json()).then((data)=>{
+          fetch('http://127.0.0.1:8000/search?' + new URLSearchParams({ query: e.target.value})).then((res) => res.json()).then((data)=>{
             console.log(data);
+            setSearchResults(data as SearchResults[]);
           });
         } } />
+        <div className="results">
+          {searchResults.map((result , idx) => 
+            <div key ={idx} className="result">
+              <h2 className="resultName">{result.game.name}</h2>
+              <img src={result.game.header_image}/>
+            </div>)}
+        </div>
         </div>
       </main>
 
